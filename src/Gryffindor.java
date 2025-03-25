d56c90c206c1d40ae3168212c05474caa50edbc5
Homework1 <danisholha760@gmail.com>
2025-03-24 23:41:32 -0500

    Comparison methods added

--- src/Gryffindor.java
+++ src/Gryffindor.java
@@ -8,6 +8,7 @@ public class Gryffindor extends Hogwarts{
         this.g1 = g1;
         this.g2 = g2;
         this.g3 = g3;
+        checkingValuesG(new int[]{g1, g2, g3});
 
         System.out.println("Nobility = " + g1 + " points. Honor = " + g2 + " points. Courage = " + g3 + " points.");
     }
@@ -35,4 +36,30 @@ public class Gryffindor extends Hogwarts{
     public void setG3(int g3) {
         this.g3 = g3;
     }
+
+    public void checkingValuesG(int [] g){
+        int [] values = {g1, g2, g3};
+        for(int i = 0; i < values.length; i++){
+            if (values[i] < 0 || values[i] > 100){
+                throw new RuntimeException("Invalid value");
+            }
+        }
+    }
+
+    public static void comparison2StudentsGryffindor(Gryffindor student1, Gryffindor student2){
+        int sumPower1 = student1.g1 + student1.g2 + student1.g3;
+        int sumPower2 = student2.g1 + student2.g2 + student2.g3;
+        if(sumPower1 > sumPower2){
+            System.out.println(student1.getName() + " - total points = " + sumPower1
+                    + " and " + student2.getName() + " - total points = " + sumPower2
+                    + ". " + student1.getName() + " is a better student in Gryffindor than " + student2.getName() + ".");
+        } else if (sumPower1 < sumPower2){
+            System.out.println(student1.getName() + " - total points = " + sumPower1
+                    + " and " + student2.getName() + " - total points = " + sumPower2
+                    + ". " + student2.getName() + " is a better student in Gryffindor than " + student1.getName() + ".");
+        } else {
+            System.out.println(student1.getName() + " and " + student2.getName() + " have the same number of points.");
+        }
+    }
+
 }
--- src/Hogwarts.java
+++ src/Hogwarts.java
@@ -4,6 +4,8 @@ public class Hogwarts {
     private int transgression;
 
     public Hogwarts(String name, int witchcraft, int transgression) {
+        checkingValues1(witchcraft);
+        checkingValues2(transgression);
         this.name = name;
         this.witchcraft = witchcraft;
         this.transgression = transgression;
@@ -33,5 +35,30 @@ public class Hogwarts {
 
     public void setTransgression(int transgression) {
         this.transgression = transgression;
+    }
+    public void checkingValues1(int witchcraft) {
+        if(witchcraft < 0 || witchcraft > 100) {
+            throw new RuntimeException("Invalid value");
+        }
+    }
+    public void checkingValues2(int transgression) {
+        if(transgression < 0 || transgression > 100) {
+            throw new RuntimeException("Invalid value");
+        }
+    }
+    public static void comparison2StudentsHogwarts(Hogwarts student1, Hogwarts student2){
+        int sumPower1 = student1.witchcraft + student1.transgression;
+        int sumPower2 = student2.witchcraft + student2.transgression;
+        if(sumPower1 > sumPower2){
+            System.out.println(student1.getName() + " - total points = " + sumPower1
+                    + " and " + student2.getName() + " - total points = " + sumPower2
+                    + ". " + student1.getName() + " has more magical power than  " + student2.getName() + ".");
+        } else if (sumPower1 < sumPower2){
+            System.out.println(student1.getName() + " - total points = " + sumPower1
+                    + " and " + student2.getName() + " - total points = " + sumPower2
+                    + ". " + student2.getName() + " has more magical power than  " + student1.getName() + ".");
+        } else {
+            System.out.println(student1.getName() + " and " + student2.getName() + " have the same magical powers.");
+        }
     }
 }
